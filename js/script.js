8031a6cd708746527140e74985486d14e64924f1
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-01 16:54:13 +0100

    first commit with js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function dice() {
+    const computerChoice = Math.floor(Math.random() * 6 + 1);
+    const userChoice = Math.floor(Math.random() * 6 + 1);
+
+    console.log(computerChoice);
+    console.log(userChoice);
+
+    if (computerChoice === userChoice) {
+        console.log("pareggio");
+    }
+    else if (computerChoice > userChoice) {
+        console.log("perso");
+    } else {
+        console.log("vinto");
+    }
+}
+
+
+function mail() {
+    const userEmail = "pincopallo@fjfsj.com"
+    const arrayEmail = ["cassone@gmail.com", "miamail@live.it", "palangarulez@outlook.com", "originaltemu@libero.it", "iamrealdeveloper@live.it"];
+
+
+    for (let i = 0; i < arrayEmail.length; i++) {
+        if (userEmail === arrayEmail[i]) {
+            console.log("Accesso Eseguito");
+            return;
+        }
+        if (userEmail != arrayEmail[i]) {
+            console.log("Accesso Negato");
+        }
+    }
+}
