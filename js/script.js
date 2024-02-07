71757d3eb146940c087a30cb43a295cf5d6c642e
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-07 09:32:13 +0100

    exercise completed, graphic to be modified

--- js/script.js
+++ js/script.js
@@ -1,44 +1,88 @@
 const diceElement = document.getElementById("uno");
 const mailElement = document.getElementById("due");
+const resultElement = document.querySelector("#result");
+
+const userDice = document.querySelector("#user");
+const cpuDice = document.querySelector("#cpu");
 
 diceElement.addEventListener("click", dice);
 mailElement.addEventListener("click", mail);
 
 
+
 function dice() {
 
-    const computerChoice = Math.floor(Math.random() * 6 + 1);
-    const userChoice = Math.floor(Math.random() * 6 + 1);
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("dice-game").style.display = "block";
 
-    console.log(computerChoice);
-    console.log(userChoice);
+    const playBtnElement = document.getElementById("play");
 
-    if (computerChoice === userChoice) {
-        console.log("pareggio");
-    }
-    else if (computerChoice > userChoice) {
-        console.log("perso");
-    } else {
-        console.log("vinto");
-    }
+    playBtnElement.addEventListener("click", function () {
+
+        const computerChoice = Math.floor(Math.random() * 6 + 1);
+        const userChoice = Math.floor(Math.random() * 6 + 1);
+
+        const diceFaces = ['<i class="fa-solid fa-dice-one"></i>', '<i class="fa-solid fa-dice-two"></i>', '<i class="fa-solid fa-dice-three"></i>', '<i class="fa-solid fa-dice-four"></i>', '<i class="fa-solid fa-dice-five"></i>', '<i class="fa-solid fa-dice-six"></i>'];
+
+        const diceFacesCpu = diceFaces[computerChoice - 1];
+        const diceFacesPlayer = diceFaces[userChoice - 1];
+
+        userDice.innerHTML = diceFacesPlayer;
+        cpuDice.innerHTML = diceFacesCpu;
+
+        playBtnElement.innerText = "Gioca Ancora"
+
+        const gameResult = document.getElementById("result-game");
+
+        if (computerChoice === userChoice) {
+            gameResult.innerHTML = "Pareggio!"
+        }
+        else if (computerChoice > userChoice) {
+            gameResult.innerHTML = "Hai Perso!"
+        } else {
+            gameResult.innerHTML = "Hai Vinto!"
+        }
+    })
 }
 
 function mail() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("mail-app").style.display = "block";
+
+    const buttonMailElement = document.querySelector("#btnMail");
+    const userMailElement = document.querySelector("#user-input");
     const arrayEmail = ["primo@gmail.com", "secondo@live.it", "terzo@outlook.com", "quarto@libero.it", "quinto@live.it"];
 
+    buttonMailElement.addEventListener("click", function () {
+        let emailSearch = false;
+        for (let i = 0; i < arrayEmail.length && !emailSearch; i++) {
 
-    let emailSearch = false;
-    for (let i = 0; i < arrayEmail.length && !emailSearch; i++) {
-
-        if (userEmail === arrayEmail[i]) {
-            emailSearch = true;
+            if (userMailElement.value === arrayEmail[i]) {
+                emailSearch = true;
+            }
         }
-    }
 
-    if (emailSearch === true) {
-        console.log("email trovata");
-    }
-    else {
-        console.log("email non trovata");
-    }
+        if (emailSearch === true) {
+            resultElement.innerHTML = "Mail trovata nel database, accesso eseguito";
+        }
+        else {
+            resultElement.innerHTML = `La Mail '${userMailElement.value}' non risulta nei nostri sistemi, registrati nel form qui sotto`;
+            const regForm = document.createElement("button");
+            regForm.classList.add("btn-success", "btn");
+            regForm.innerHTML = "Registrati"
+            document.querySelector("#reg-button").append(regForm);
+        }
+    })
 }
+
+const btnReload1 = document.querySelector("#btn_reload1");
+const btnReload2 = document.querySelector("#btn_reload2");
+
+btnReload1.addEventListener("click", function () {
+    window.location.reload();
+})
+
+btnReload2.addEventListener("click", function () {
+    window.location.reload();
+})
+
